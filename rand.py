eb6f4800712bbd12240986fa888d510b981b0fa8
cwleungaa <cwleungaa@gmail.com>
2019-02-20 03:47:43 +0800

    Second Version

--- rand.py
+++ rand.py
@@ -2,6 +2,8 @@ import random
 
 r = random.randint(1,100)
 
+i = 1
+
 while True:
 
 	guess = input('請猜一數字: ')
@@ -10,10 +12,12 @@ while True:
 
 	if guess == r :
 		print('終於猜對了')
+		print('你共猜了',i,'次')
 		break
 
 	elif guess < r:
 		print('你猜的數字比答案還少')
-
+		i = i + 1
 	else:
 		print('你猜的數字比答案還大')
+		i = i + 1
